e1f00c5cf68fc76a3074dd946499feaf1fe5de66
amira_spokn001 <amira@getspokn.com>
2026-01-26 15:48:29 +0200

    fix: login

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -52,8 +52,8 @@ export default function SignInForm() {
       const response = await adminLogin({ email, password });
       setLoading(false)
       if (response.StatusCode == 200) {
-        localStorage.setItem('GMadminData', response.Data);
-        localStorage.setItem('GMadminToken', JSON.stringify(response.Data?.token));
+        localStorage.setItem('GMadminData', JSON.stringify(response.Data));
+        localStorage.setItem('GMadminToken', response.Data?.token );
         window.location.href='/videos'
         // navigate('/videos');
       }
