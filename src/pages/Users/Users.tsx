69821c7d9aad63d5e7ff150679c0f15d06931585
sayedosama7 <sayedosama088@gmail.com>
2026-02-04 17:16:19 +0200

    update levels name

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -8,6 +8,7 @@ import {
   fetchUserPermissions,
   hasPermission,
 } from "../../utils/permissions/permissions";
+import PageMeta from "../../components/common/PageMeta";
 
 export default function Users() {
   const { t } = useLanguage();
@@ -65,59 +66,62 @@ export default function Users() {
   };
 
   return (
-    <div className="relative rounded-2xl border-b border-[#D9D9D9] pb-5  dark:border-gray-800  dark:bg-neutral-800 bg-[#EDEDED] min-h-[calc(100vh-48px)]">
-      <div className="h-[70px] mb-6 flex flex-wrap items-center justify-between gap-4 px-5 border-b border-[#D9D9D9] dark:border-gray-600 py-4">
-        <h2 className="text-xl font-bold text-gray-900 dark:text-white">
-          {t("pageTitle")}
-        </h2>
-      </div>
-      <div className="h-[80%] px-5">
-        <div className="flex justify-between items-center flex-col md:flex-row gap-4 mb-8 ">
-          <div className="flex items-center gap-4">
-            <button
-              className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
-                activeTab === "tab1"
-                  ? "bg-linear-to-r from-primary to-secondary text-white"
-                  : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
-              }`}
-              onClick={() => setActiveTab("tab1")}
-            >
-              {t("adminsTab")}
-            </button>
-            <button
-              className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
-                activeTab === "tab2"
-                  ? "bg-linear-to-r from-primary to-secondary text-white"
-                  : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
-              }`}
-              onClick={() => setActiveTab("tab2")}
-            >
-              {t("parentsTab")}
-            </button>
-            <button
-              className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
-                activeTab === "tab3"
-                  ? "bg-linear-to-r from-primary to-secondary text-white"
-                  : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
-              }`}
-              onClick={() => setActiveTab("tab3")}
-            >
-              {t("childrenTab")}
-            </button>
+    <div>
+      <PageMeta title="Green minds Admin | Users" description={``} />
+      <div className="relative rounded-2xl border-b border-[#D9D9D9] pb-5  dark:border-gray-800  dark:bg-neutral-800 bg-[#EDEDED] min-h-[calc(100vh-48px)]">
+        <div className="h-[70px] mb-6 flex flex-wrap items-center justify-between gap-4 px-5 border-b border-[#D9D9D9] dark:border-gray-600 py-4">
+          <h2 className="text-xl font-bold text-gray-900 dark:text-white">
+            {t("pageTitle")}
+          </h2>
+        </div>
+        <div className="h-[80%] px-5">
+          <div className="flex justify-between items-center flex-col md:flex-row gap-4 mb-8 ">
+            <div className="flex items-center gap-4">
+              <button
+                className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
+                  activeTab === "tab1"
+                    ? "bg-linear-to-r from-primary to-secondary text-white"
+                    : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
+                }`}
+                onClick={() => setActiveTab("tab1")}
+              >
+                {t("adminsTab")}
+              </button>
+              <button
+                className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
+                  activeTab === "tab2"
+                    ? "bg-linear-to-r from-primary to-secondary text-white"
+                    : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
+                }`}
+                onClick={() => setActiveTab("tab2")}
+              >
+                {t("parentsTab")}
+              </button>
+              <button
+                className={`px-3 md:px-6 py-2 font-medium text-base rounded-xl ${
+                  activeTab === "tab3"
+                    ? "bg-linear-to-r from-primary to-secondary text-white"
+                    : "bg-[#FAFAFA] text-black dark:bg-[#2f3131] dark:text-white dark:border-gray-800 border border-[#EDEDED] hover:text-white hover:bg-[#25B16F]"
+                }`}
+                onClick={() => setActiveTab("tab3")}
+              >
+                {t("childrenTab")}
+              </button>
+            </div>
+
+            {canAdd[activeTab] && (
+              <button
+                onClick={handleAdd}
+                className="flex items-center gap-2 border border-[#25B16F] text-[#25B16F] px-4 py-2 rounded-lg font-medium text-base hover:bg-[#25B16F] hover:text-white transition duration-200"
+              >
+                <Plus className="w-5 h-5" />
+                {addButtonText[activeTab]}
+              </button>
+            )}
           </div>
 
-          {canAdd[activeTab] && (
-            <button
-              onClick={handleAdd}
-              className="flex items-center gap-2 border border-[#25B16F] text-[#25B16F] px-4 py-2 rounded-lg font-medium text-base hover:bg-[#25B16F] hover:text-white transition duration-200"
-            >
-              <Plus className="w-5 h-5"/>
-              {addButtonText[activeTab]}
-            </button>
-          )}
+          <div>{tabContent[activeTab]}</div>
         </div>
-
-        <div>{tabContent[activeTab]}</div>
       </div>
     </div>
   );
